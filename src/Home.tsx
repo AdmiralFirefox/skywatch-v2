c83767eceb6df4a4cb5b1772942ed3c0448130a3
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-10-23 20:23:36 +0800

    Added assets and home page content

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,28 @@
-import "./styles/Home.scss";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import WebLogo from "./assets/web-logo.png";
+import styles from "./styles/Home.module.scss";
 
 function Home() {
+  useEffect(() => {
+    document.getElementsByTagName("body")[0].className = styles["home-body"];
+
+    return () => {
+      document.getElementsByTagName("body")[0].className = "";
+    };
+  }, []);
+
   return (
     <main>
-      <h1>Home Page</h1>
+      <section className={styles["home-card"]}>
+        <img src={WebLogo} alt="Web Logo" />
+        <h1>SkyWatch: Global Weather Monitoring System</h1>
+        <p>
+          A weather application to know the weather of a specific place, either
+          be a city or country.
+        </p>
+        <Link to="/search">Start Seaching</Link>
+      </section>
     </main>
   );
 }
