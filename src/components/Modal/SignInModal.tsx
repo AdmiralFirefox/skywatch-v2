a439917520833adef2bf699247d6ce72ca4ac0c3
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-10-30 17:16:46 +0800

    Added animations on modals

--- src/components/Modal/SignInModal.tsx
+++ src/components/Modal/SignInModal.tsx
@@ -2,6 +2,7 @@ import { useWindowSize } from "../../hooks/useWindowSize";
 import GoogleLogo from "../../assets/icons/google.png";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { IconContext } from "react-icons";
+import { motion, AnimatePresence } from "framer-motion";
 import styles from "../../styles/modal/SignInModal.module.scss";
 
 interface SignInModalProps {
@@ -19,34 +20,51 @@ const SignInModal = ({
 
   return (
     <>
-      {signInModal && (
-        <div
-          className={styles["backdrop"]}
-          style={{ height: windowHeight }}
-          onClick={closeSignInModal}
-        ></div>
-      )}
+      <AnimatePresence>
+        {signInModal && (
+          <motion.div
+            className={styles["backdrop"]}
+            style={{ height: windowHeight }}
+            onClick={closeSignInModal}
+            key="sign-in-backdrop"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+          ></motion.div>
+        )}
+      </AnimatePresence>
 
-      {signInModal && (
-        <div
-          className={styles["modal"]}
-          style={{ maxHeight: `calc(${windowHeight}px - 10vh)` }}
-        >
-          <button className={styles["close-button"]} onClick={closeSignInModal}>
-            <IconContext.Provider value={{ className: styles["icon"] }}>
-              <AiFillCloseCircle />
-            </IconContext.Provider>
-          </button>
-          <h1>Sign In with Google</h1>
-          <p>Sign in to use the various features of SkyWatch.</p>
-          <button
-            onClick={signInWithgoogle}
-            className={styles["google-button"]}
+      <AnimatePresence>
+        {signInModal && (
+          <motion.div
+            className={styles["modal"]}
+            style={{ maxHeight: `calc(${windowHeight}px - 10vh)` }}
+            key="sign-in-modal"
+            initial={{ opacity: 0, top: "45%" }}
+            animate={{ opacity: 1, top: "50%" }}
+            exit={{ opacity: 0, top: "45%" }}
+            transition={{ type: "spring", duration: 0.6 }}
           >
-            Sign In <img src={GoogleLogo} alt="Google Logo" />
-          </button>
-        </div>
-      )}
+            <button
+              className={styles["close-button"]}
+              onClick={closeSignInModal}
+            >
+              <IconContext.Provider value={{ className: styles["icon"] }}>
+                <AiFillCloseCircle />
+              </IconContext.Provider>
+            </button>
+            <h1>Sign In with Google</h1>
+            <p>Sign in to use the various features of SkyWatch.</p>
+            <button
+              onClick={signInWithgoogle}
+              className={styles["google-button"]}
+            >
+              Sign In <img src={GoogleLogo} alt="Google Logo" />
+            </button>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
--- src/components/Modal/SignOutModal.tsx
+++ src/components/Modal/SignOutModal.tsx
@@ -2,6 +2,7 @@ import { useWindowSize } from "../../hooks/useWindowSize";
 import { FaSignOutAlt } from "react-icons/fa";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { IconContext } from "react-icons";
+import { motion, AnimatePresence } from "framer-motion";
 import styles from "../../styles/modal/SignOutModal.module.scss";
 
 interface SignOutModalProps {
@@ -25,45 +26,60 @@ const SignOutModal = ({
 
   return (
     <>
-      {signOutModal && (
-        <div
-          className={styles["backdrop"]}
-          style={{ height: windowHeight }}
-          onClick={closeSignOutModal}
-        ></div>
-      )}
+      <AnimatePresence>
+        {signOutModal && (
+          <motion.div
+            className={styles["backdrop"]}
+            style={{ height: windowHeight }}
+            onClick={closeSignOutModal}
+            key="sign-out-backdrop"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+          ></motion.div>
+        )}
+      </AnimatePresence>
 
-      {signOutModal && (
-        <div
-          className={styles["modal"]}
-          style={{ maxHeight: `calc(${windowHeight}px - 10vh)` }}
-        >
-          <button className={styles["close-button"]} onClick={closeSignOutModal}>
-            <IconContext.Provider value={{ className: styles["icon"] }}>
-              <AiFillCloseCircle />
-            </IconContext.Provider>
-          </button>
-
-          <div className={styles["user-info"]}>
-            <img src={userPhoto} alt="User Photo" />
-            <div className={styles["content"]}>
-              <p>{userName}</p>
-              <p>{userEmail}</p>
-            </div>
-          </div>
-
-          <div className={styles["sign-out-button"]}>
-            <button onClick={signOutAccount}>
-              Sign Out{" "}
-              <IconContext.Provider
-                value={{ className: styles["icon"] }}
-              >
-                <FaSignOutAlt />
+      <AnimatePresence>
+        {signOutModal && (
+          <motion.div
+            className={styles["modal"]}
+            style={{ maxHeight: `calc(${windowHeight}px - 10vh)` }}
+            key="sign-out-modal"
+            initial={{ opacity: 0, top: "45%" }}
+            animate={{ opacity: 1, top: "50%" }}
+            exit={{ opacity: 0, top: "45%" }}
+            transition={{ type: "spring", duration: 0.6 }}
+          >
+            <button
+              className={styles["close-button"]}
+              onClick={closeSignOutModal}
+            >
+              <IconContext.Provider value={{ className: styles["icon"] }}>
+                <AiFillCloseCircle />
               </IconContext.Provider>
             </button>
-          </div>
-        </div>
-      )}
+
+            <div className={styles["user-info"]}>
+              <img src={userPhoto} alt="User Photo" />
+              <div className={styles["content"]}>
+                <p>{userName}</p>
+                <p>{userEmail}</p>
+              </div>
+            </div>
+
+            <div className={styles["sign-out-button"]}>
+              <button onClick={signOutAccount}>
+                Sign Out{" "}
+                <IconContext.Provider value={{ className: styles["icon"] }}>
+                  <FaSignOutAlt />
+                </IconContext.Provider>
+              </button>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
