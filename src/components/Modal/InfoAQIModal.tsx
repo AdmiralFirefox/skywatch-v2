4bdc82d9cae0a39f9c54e617e4fc02944e1e8ef7
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-10-31 21:37:59 +0800

    Added info aqi modal

---
+++ src/components/Modal/InfoAQIModal.tsx
@@ -0,0 +1,67 @@
+import { useWindowSize } from "../../hooks/useWindowSize";
+import { motion, AnimatePresence } from "framer-motion";
+import styles from "../../styles/modal/InfoAQIModal.module.scss";
+
+interface InfoAQIModalProps {
+  infoModal: boolean;
+  closeInfoModal: () => void;
+}
+
+const InfoAQIModal = ({ infoModal, closeInfoModal }: InfoAQIModalProps) => {
+  const { height: windowHeight } = useWindowSize();
+
+  return (
+    <>
+      <AnimatePresence>
+        {infoModal && (
+          <motion.div
+            className={styles["backdrop"]}
+            style={{ height: windowHeight }}
+            onClick={closeInfoModal}
+            key="info-aqi-backdrop"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+          ></motion.div>
+        )}
+      </AnimatePresence>
+
+      <AnimatePresence>
+        {infoModal && (
+          <motion.div
+            className={styles["modal"]}
+            style={{ maxHeight: `calc(${windowHeight}px - 10vh)` }}
+            key="info-aqi-modal"
+            initial={{ opacity: 0, top: "45%" }}
+            animate={{ opacity: 1, top: "50%" }}
+            exit={{ opacity: 0, top: "45%" }}
+            transition={{ type: "spring", duration: 0.6 }}
+          >
+            <h1>What is AQI?</h1>
+            <p>
+              The Air Quality Index (AQI) is a system that is used to tell the
+              air quality of a place. It tells you how clean or polluted is the
+              air. The AQI can be categorized into the following:
+            </p>
+            <p>
+              1 - Good <br />
+              2 - Fair <br />
+              3 - Moderate <br />
+              4 - Poor <br />
+              5 - Very Poor <br />
+            </p>
+            <button
+              onClick={closeInfoModal}
+              className={styles["google-button"]}
+            >
+              Close
+            </button>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
+  );
+};
+
+export default InfoAQIModal;
--- src/components/Search/WeatherAQI.tsx
+++ src/components/Search/WeatherAQI.tsx
@@ -1,20 +1,48 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
+import InfoAQIModal from "../Modal/InfoAQIModal";
 import { WeatherAQIContext } from "../../context/WeatherAQIContext";
 import BeatLoader from "react-spinners/BeatLoader";
+import { AiFillInfoCircle } from "react-icons/ai";
+import { IconContext } from "react-icons";
 import styles from "../../styles/search/WeatherAQI.module.scss";
+
+interface CloseIconProps {
+  openInfoModal: () => void;
+}
+
+const CloseIcon = ({ openInfoModal }: CloseIconProps) => {
+  return (
+    <button className={styles["close-button"]} onClick={openInfoModal}>
+      <IconContext.Provider value={{ className: styles["icon"] }}>
+        <AiFillInfoCircle />
+      </IconContext.Provider>
+    </button>
+  );
+};
 
 const WeatherAQI = () => {
   const { aqiMainData, aqiLoading, aqiError } = useContext(WeatherAQIContext);
+  const [infoModal, setInfoModal] = useState(false);
+
+  const openInfoModal = () => {
+    setInfoModal(true);
+  };
+
+  const closeInfoModal = () => {
+    setInfoModal(false);
+  };
 
   return (
     <>
       {aqiLoading ? (
         <div className={styles["loading"]}>
+          <CloseIcon openInfoModal={openInfoModal} />
           <p>Air Quality Index</p>
           <BeatLoader color="#18425A" size={12} />
         </div>
       ) : aqiError ? (
         <div className={styles["error"]}>
+          <CloseIcon openInfoModal={openInfoModal} />
           <p>Air Quality Index</p>
           <p>---</p>
         </div>
@@ -22,27 +50,33 @@ const WeatherAQI = () => {
         <>
           {aqiMainData === 1 || aqiMainData === 2 ? (
             <div className={styles["air-quality-index-good"]}>
+              <CloseIcon openInfoModal={openInfoModal} />
               <p>Air Quality Index</p>
               <p>{aqiMainData}</p>
             </div>
           ) : aqiMainData === 3 ? (
             <div className={styles["air-quality-index-fair"]}>
+              <CloseIcon openInfoModal={openInfoModal} />
               <p>Air Quality Index</p>
               <p>{aqiMainData}</p>
             </div>
           ) : aqiMainData === 4 ? (
             <div className={styles["air-quality-index-poor"]}>
+              <CloseIcon openInfoModal={openInfoModal} />
               <p>Air Quality Index</p>
               <p>{aqiMainData}</p>
             </div>
           ) : (
             <div className={styles["air-quality-index-very-poor"]}>
+              <CloseIcon openInfoModal={openInfoModal} />
               <p>Air Quality Index</p>
               <p>{aqiMainData}</p>
             </div>
           )}
         </>
       )}
+
+      <InfoAQIModal infoModal={infoModal} closeInfoModal={closeInfoModal} />
     </>
   );
 };
