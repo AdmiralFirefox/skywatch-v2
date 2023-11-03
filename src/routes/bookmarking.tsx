c98cf0875dec4473044d9613aa1f78446a232bf7
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-11-03 11:14:29 +0800

    Added background and styling on bookmarking page

--- src/routes/bookmarking.tsx
+++ src/routes/bookmarking.tsx
@@ -1,11 +1,26 @@
+import { useEffect } from "react";
 import Navbar from "../components/Navbar/Navbar";
+import styles from "../styles/bookmarking/Bookmarking.module.scss";
 
 const Bookmarking = () => {
+  // Main Background
+  useEffect(() => {
+    document.getElementsByTagName("body")[0].className = styles["main-bg"];
+
+    return () => {
+      document.getElementsByTagName("body")[0].className = "";
+    };
+  }, []);
+
   return (
     <>
       <Navbar />
 
-      <main></main>
+      <main className={styles["bookmark-card"]}>
+        <div className={styles["header"]}>
+          <h1>Your Bookmarks</h1>
+        </div>
+      </main>
     </>
   );
 };
