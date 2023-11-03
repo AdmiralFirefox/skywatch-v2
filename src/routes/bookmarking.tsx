75e5291510b261a1a1ab6aed2a3bfe6665af2a58
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-11-03 19:47:18 +0800

    Fetched bookmark data from firestore, added content and styling and handling loading states

--- src/routes/bookmarking.tsx
+++ src/routes/bookmarking.tsx
@@ -1,8 +1,72 @@
-import { useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { db } from "../firebase/firebase";
+import { AuthContext } from "../context/AuthContext";
+import { useAppDispatch } from "../app/redux_hooks";
+import { setSearchValue } from "../features/search/searchSlice";
+import SignInImage from "../assets/icons/sign-in.png";
+import FavoritesImage from "../assets/icons/magnifying-glass.png";
 import Navbar from "../components/Navbar/Navbar";
+import Loading from "../components/Placeholder/Loading";
+import Placeholder from "../components/Placeholder/Placeholder";
+import { AiFillHeart } from "react-icons/ai";
+import { IconContext } from "react-icons";
+import { BookmarProps } from "../types/BookmarkTypes";
 import styles from "../styles/bookmarking/Bookmarking.module.scss";
 
 const Bookmarking = () => {
+  const user = useContext(AuthContext);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const [bookmarks, setBookmarks] = useState<BookmarProps[]>([]);
+  const [loadingBookmarks, setLoadingBookmarks] = useState(false);
+
+  const goToSearchPage = (searchedPlace: string | undefined) => {
+    navigate("/search");
+    dispatch(setSearchValue(searchedPlace as string));
+  };
+
+  // Deleting a country from bookmarks
+  const deleteBookmarkedCountry = async (docId: string) => {
+    const docRef = doc(db, "bookmarks", docId);
+    await deleteDoc(docRef);
+  };
+
+  const handleDelete = (docId: string) => {
+    deleteBookmarkedCountry(docId).then(() => {
+      setBookmarks(bookmarks.filter((bookmark) => bookmark.id !== docId));
+    });
+  };
+
+  // Getting bookmarks from the collection
+  useEffect(() => {
+    setLoadingBookmarks(true);
+    if (user) {
+      const bookmarksRef = collection(db, "bookmarks");
+      const q = query(bookmarksRef, orderBy("time_added", "desc"));
+
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        const bookmarks = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setBookmarks(bookmarks);
+        setLoadingBookmarks(false);
+      });
+
+      return () => unsubscribe();
+    }
+  }, [user]);
+
   // Main Background
   useEffect(() => {
     document.getElementsByTagName("body")[0].className = styles["main-bg"];
@@ -12,15 +76,59 @@ const Bookmarking = () => {
     };
   }, []);
 
+  if (!user) {
+    return (
+      <>
+        <Navbar />
+        <Placeholder
+          image={SignInImage}
+          title="You are not signed in."
+          subtitle="Please sign in to use the bookmarking feature of SkyWatch."
+        />
+      </>
+    );
+  }
+
   return (
     <>
       <Navbar />
 
-      <main className={styles["bookmark-card"]}>
-        <div className={styles["header"]}>
-          <h1>Your Bookmarks</h1>
-        </div>
-      </main>
+      {bookmarks.length === 0 && !loadingBookmarks ? (
+        <Placeholder
+          image={FavoritesImage}
+          title="You have no recent bookmarks."
+          subtitle="Try searching for a city or country and bookmark them."
+        />
+      ) : loadingBookmarks ? (
+        <Loading />
+      ) : (
+        <main className={styles["bookmark-card"]}>
+          <div className={styles["header"]}>
+            <h1>Your Bookmarks</h1>
+          </div>
+
+          <ul className={styles["card-container"]}>
+            {bookmarks.map((bookmark) => (
+              <li key={bookmark.id} className={styles["card"]}>
+                <button
+                  onClick={() => goToSearchPage(bookmark.place)}
+                  className={styles["place-button"]}
+                >
+                  <h1>{bookmark.place}</h1>
+                </button>
+                <button
+                  className={styles["icon-button"]}
+                  onClick={() => handleDelete(bookmark.id)}
+                >
+                  <IconContext.Provider value={{ className: styles["icon"] }}>
+                    <AiFillHeart />
+                  </IconContext.Provider>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </main>
+      )}
     </>
   );
 };
---
+++ src/types/BookmarkTypes.tsx
@@ -0,0 +1,7 @@
+export interface BookmarProps {
+  id: string;
+  place?: string;
+  time_added?: {
+    seconds: number;
+  };
+}
