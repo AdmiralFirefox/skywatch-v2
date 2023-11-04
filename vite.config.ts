e0ba4e90b1ed937eed87732e558914fe0567bb91
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-11-05 03:27:30 +0800

    Added set-up for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        theme_color: "#49445a",
+        background_color: "#49445a",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        name: "SkyWatch",
+        short_name: "SkyWatch",
+        description:
+          "A weather application to know the weather of a specific place, either be a city or country.",
+        icons: [
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
