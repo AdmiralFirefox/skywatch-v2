93048cb31db39957d0ebe22fb2c9520e0311d4b3
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-11-04 19:06:14 +0800

    Added keyboard events on autofill

--- src/components/Search/AutoComplete.tsx
+++ src/components/Search/AutoComplete.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, useRef } from "react";
 import { UseQueryResult, useQuery } from "@tanstack/react-query";
 import { fetchAutoCompleteData } from "../../utils/fetchAutoCompleteData";
 import { useWindowSize } from "../../hooks/useWindowSize";
@@ -12,12 +13,16 @@ interface AutoCompleteDataTypes {
   }[];
 }
 
+type AutoCompleteDataRef = { name: string; country: string; region: string };
+
 interface AutoCompleteProps {
   searchPlace: string;
   selectCountry: (selectedPlace: string) => void;
 }
 
 const AutoComplete = ({ searchPlace, selectCountry }: AutoCompleteProps) => {
+  const [focusedButtonIndex, setFocusedButtonIndex] = useState(-1);
+  const focusedCountry = useRef<AutoCompleteDataRef | null | undefined>(null);
   const { height } = useWindowSize();
 
   const {
@@ -34,6 +39,39 @@ const AutoComplete = ({ searchPlace, selectCountry }: AutoCompleteProps) => {
     staleTime: 30000,
     enabled: Boolean(searchPlace),
   });
+
+  useEffect(() => {
+    const handleKeyDown = (event: {
+      key: string;
+      preventDefault: () => void;
+    }) => {
+      if (event.key === "ArrowDown") {
+        setFocusedButtonIndex((prevIndex) => {
+          const nextIndex = (prevIndex + 1) % data!.data.length;
+          focusedCountry.current = data?.data[nextIndex];
+          return nextIndex;
+        });
+      } else if (event.key === "ArrowUp") {
+        setFocusedButtonIndex((prevIndex) => {
+          const nextIndex =
+            (prevIndex - 1 + data!.data.length) % data!.data.length;
+          focusedCountry.current = data?.data[nextIndex];
+          return nextIndex;
+        });
+      } else if (event.key === "Enter" && focusedCountry.current) {
+        event.preventDefault();
+        selectCountry(
+          `${focusedCountry.current.name}, ${focusedCountry.current.country}`
+        );
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [data, data?.data, data?.data.length, selectCountry]);
 
   return (
     <>
@@ -53,7 +91,12 @@ const AutoComplete = ({ searchPlace, selectCountry }: AutoCompleteProps) => {
               {data?.data.map((country, i) => (
                 <button
                   key={i}
-                  className={styles["select-button"]}
+                  style={{ maxHeight: `calc(${height}px - 47vh)` }}
+                  className={
+                    i === focusedButtonIndex
+                      ? styles["select-button-active"]
+                      : styles["select-button"]
+                  }
                   onClick={() =>
                     selectCountry(`${country.name}, ${country.country}`)
                   }
